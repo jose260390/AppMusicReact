db4a42b0323332b630c680c7485fac6a82ae5e6a
jose260390 <jose.bravo.honores90@gmail.com>
2025-08-03 19:13:16 -0400

    chore: minor adjustments before new feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Sidebar from './components/AppShell/Sidebar.jsx';
 import Header from './components/AppShell/Header.jsx';
 import PlayerBar from './components/Player/PlayerBar.jsx';
